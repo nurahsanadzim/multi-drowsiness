dc9cc59f0469ab89f8c03d2ea09b4b13e604ecb3
nurahsanadzim <nurahsanadzim@gmail.com>
2021-01-26 18:06:37 +0800

    added MAR from mauckc code

--- multi_drowsiness.py
+++ multi_drowsiness.py
@@ -1,23 +1,15 @@
-# USAGE
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
-
 # import the necessary packages
 from scipy.spatial import distance as dist
-from imutils.video import VideoStream
+from imutils.video import VideoStream, FPS
 from imutils import face_utils
 from threading import Thread
 import numpy as np
-import playsound
 import argparse
 import imutils
 import time
 import dlib
 import cv2
 
-def sound_alarm(path):
-	# play an alarm sound
-	playsound.playsound(path)
 
 def eye_aspect_ratio(eye):
 	# compute the euclidean distances between the two sets of
@@ -34,23 +26,39 @@ def eye_aspect_ratio(eye):
 
 	# return the eye aspect ratio
 	return ear
- 
+
+def mouth_aspect_ratio(mouth):
+	# compute the euclidean distances between the two sets of
+	# vertical mouth landmarks (x, y)-coordinates
+	A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
+	B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
+
+	# compute the euclidean distance between the horizontal
+	# mouth landmark (x, y)-coordinates
+	C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
+
+	# compute the mouth aspect ratio
+	mar = (A + B) / (2.0 * C)
+
+	# return the mouth aspect ratio
+	return mar
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-ap.add_argument("-a", "--alarm", type=str, default="",
-	help="path alarm .WAV file")
-ap.add_argument("-w", "--webcam", type=int, default=0,
-	help="index of webcam on system")
+ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
+ap.add_argument("-w", "--webcam", type=int, default=0, help="index of webcam on system")
+ap.add_argument("-v", "--video", required=True, help="path to input video file")
 args = vars(ap.parse_args())
  
 # define two constants, one for the eye aspect ratio to indicate
 # blink and then a second constant for the number of consecutive
 # frames the eye must be below the threshold for to set off the
 # alarm
-EYE_AR_THRESH = 0.3
+EYE_AR_THRESH = 0.15
 EYE_AR_CONSEC_FRAMES = 48
+
+# define one constants, for mouth aspect ratio to indicate open mouth
+MOUTH_AR_THRESH = 0.83
 
 # initialize the frame counter as well as a boolean used to
 # indicate if the alarm is going off
@@ -68,18 +76,32 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+# grab the indexes of the facial landmarks for the mouth
+(mStart, mEnd) = (49, 68)
+
 # start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = VideoStream(src=args["webcam"]).start()
-time.sleep(1.0)
+# print("[INFO] starting video stream thread...")
+# vs = VideoStream(src=args["webcam"]).start().sleep(1.0)
+vs = cv2.VideoCapture(args["video"])
+fps = FPS().start()
 
 # loop over frames from the video stream
 while True:
+	# grab the frame from the threaded video file stream
+	(grabbed, frame) = vs.read()
+	
+	# if the frame was not grabbed, then we have reached the end of the stream
+	if not grabbed:
+		break
+
 	# grab the frame from the threaded video file stream, resize
 	# it, and convert it to grayscale
 	# channels)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=675)
+	_, frame = vs.read()
+	(w, h, c) = frame.shape
+	#syntax: cv2.resize(img, (width, height))
+	frame = cv2.resize(frame,(400, h))
+
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	# detect faces in the grayscale frame
@@ -88,7 +110,6 @@ while True:
 	# mycode
 	# inisialisasi jumlah deteksi wajah
 	index = 0
-
 
 	# loop over the face detections
 	for rect in rects:
@@ -105,6 +126,10 @@ while True:
 		rightEye = shape[rStart:rEnd]
 		leftEAR = eye_aspect_ratio(leftEye)
 		rightEAR = eye_aspect_ratio(rightEye)
+
+		# extract the mouth coordinates, then use the
+		# coordinates to compute the mouth aspect ratio
+		mouth = shape[mStart:mEnd]
 
 		# average the eye aspect ratio together for both eyes
 		ear = (leftEAR + rightEAR) / 2.0
@@ -128,15 +153,6 @@ while True:
 				if not ALARM_ON:
 					ALARM_ON = True
 
-					# check to see if an alarm file was supplied,
-					# and if so, start a thread to have the alarm
-					# sound played in the background
-					# if args["alarm"] != "":
-					# 	t = Thread(target=sound_alarm,
-					# 		args=(args["alarm"],))
-					# 	t.deamon = True
-					# 	t.start()
-
 				# draw an alarm on the frame
 				cv2.putText(frame, "MENGANTUK!", (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
@@ -150,25 +166,44 @@ while True:
 		# draw the computed eye aspect ratio on the frame to help
 		# with debugging and setting the correct eye aspect ratio
 		# thresholds and frame counters
-		cv2.putText(frame, "EAR: {:.2f}: pada wajah {}".format(ear, index), (0, 30*index),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		# cv2.putText(frame, "EAR: {:.2f}: pada wajah {}".format(ear, index), (0, 30*index),
+		# 	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 		# mycode
 		# cv2.putText(frame, "Wajah: {}".format(index), (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
-
-
  
+		mouthMAR = mouth_aspect_ratio(mouth)
+		mar = mouthMAR
+		# compute the convex hull for the mouth, then
+		# visualize the mouth
+		mouthHull = cv2.convexHull(mouth)
+		
+		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+		cv2.putText(frame, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+		# Draw text if mouth is open
+		if mar > MOUTH_AR_THRESH:
+			cv2.putText(frame, "Mouth is Open!", (30,60),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+
 	# show the frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
- 
+	fps.update()
+
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
 
 # run the code
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
+# py multi_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
+# py multi_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --video 10.MOV
