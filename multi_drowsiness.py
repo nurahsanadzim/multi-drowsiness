8ae5a42e7064355682be6ea5acca2cbe9da57155
nurahsanadzim <nurahsanadzim@gmail.com>
2021-04-18 14:34:11 +0800

    maintain object ID tracked until the end of video

--- multi_drowsiness.py
+++ multi_drowsiness.py
@@ -13,21 +13,17 @@ from pyimagesearch.centroidtracker import CentroidTracker
 
 
 def eye_aspect_ratio(eye):
-	# vertical sets
-	A = dist.euclidean(eye[1], eye[5])
+	A = dist.euclidean(eye[1], eye[5]) 
 	B = dist.euclidean(eye[2], eye[4])
-	# horizontal set
 	C = dist.euclidean(eye[0], eye[3])
 	ear = (A + B) / (2.0 * C)
 	return ear
 
 def mouth_aspect_ratio(mouth):
-	# vertical sets
-	A = dist.euclidean(mouth[2], mouth[10]) # 51, 59
-	B = dist.euclidean(mouth[4], mouth[8]) # 53, 57
-	C = dist.euclidean(mouth[3], mouth[9]) # 52, 58
-	# horizontal set
-	D = dist.euclidean(mouth[0], mouth[6]) # 49, 55
+	A = dist.euclidean(mouth[2], mouth[10])
+	B = dist.euclidean(mouth[4], mouth[8])
+	C = dist.euclidean(mouth[3], mouth[9])
+	D = dist.euclidean(mouth[0], mouth[6])
 	mar = (A + B + C) / (3.0 * D)
 	return mar
 
@@ -64,11 +60,9 @@ fps = FPS().start()
 face_list = []
 exec_time_30_frame = queue.Queue(maxsize=15)
 exec_time_list = []
-
 vs_time = 0
 
 while True:
-	# waktu video (miliseconds)
 	vs_time = int(vs.get(cv2.CAP_PROP_POS_MSEC))
 
 	e1 = cv2.getTickCount()
@@ -77,9 +71,7 @@ while True:
 	
 	cv2.putText(frame, millis_convert(vs_time), (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-	# if the frame was not grabbed, then we have reached the end of the stream
 	if not grabbed:
-		print(vs_time)
 		generate_csv(exec_time_list)
 		break
 
@@ -89,123 +81,108 @@ while True:
 
 	for rect in rects:
 		(x, y, w, h) = face_utils.rect_to_bb(rect)
-
-		# input bounding box setiap deteksi
 		startX = rect.left()
 		startY = rect.top()
 		endX = rect.right()
 		endY = rect.bottom()
 		cX = int((startX + endX) / 2.0)
 		cY = int((startY + endY) / 2.0)
-
-		# kumpulkan data bounding box dan centroid setiap deteksi
 		unordered_rects.append([startX, startY, endX, endY, [cX, cY]])
 
-	# urutkan data berdasarkan objek sebelumnya (euclidean distance)
 	objects_ordered = ct.update(unordered_rects)
-	ordered_rects = []
 
 	for (objectID, centroid) in objects_ordered.items():
-		# urutkan setiap deteksi wajah pada frame saat ini
-		for u in unordered_rects:
-			if centroid[0] == u[4][0] and centroid[1] == u[4][1]:
-				rect = dlib.rectangle(u[0], u[1], u[2], u[3])
-				ordered_rects.append(rect)
-
-	for (i, rect) in enumerate(ordered_rects):
-		# cek jika list wajah belum ada
 		try:
-			face_list[i]
+			face_list[objectID]
 		except IndexError:
 			face_list.append({
 				'consec_frame': 0,
 			})
 
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+		for u in unordered_rects:
+			if centroid[0] == u[4][0] and centroid[1] == u[4][1]:
+				rect = dlib.rectangle(u[0], u[1], u[2], u[3])
 
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-		ear = (leftEAR + rightEAR) / 2.0
+				shape = predictor(gray, rect)
+				shape = face_utils.shape_to_np(shape)
 
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+				leftEye = shape[lStart:lEnd]
+				rightEye = shape[rStart:rEnd]
+				leftEAR = eye_aspect_ratio(leftEye)
+				rightEAR = eye_aspect_ratio(rightEye)
+				ear = (leftEAR + rightEAR) / 2.0
 
-		mouth = shape[mStart:mEnd]
-		mar = mouth_aspect_ratio(mouth)
+				leftEyeHull = cv2.convexHull(leftEye)
+				rightEyeHull = cv2.convexHull(rightEye)
+				cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+				cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-		mouthHull = cv2.convexHull(mouth)	
-		cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+				mouth = shape[mStart:mEnd]
+				mar = mouth_aspect_ratio(mouth)
 
-		(x, y, w, h) = face_utils.rect_to_bb(rect)
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
-		cv2.putText(frame, 'ID{} EAR:{:.2f} MAR:{:.2f}'.format(i, ear, mar), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+				mouthHull = cv2.convexHull(mouth)	
+				cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
 
-		if ear < EYE_AR_THRESH:
+				(x, y, w, h) = face_utils.rect_to_bb(rect)
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
+				cv2.putText(frame, '#{} E:{:.2f} M:{:.2f}'.format(objectID, ear, mar), (x - 10, y - 4), 
+					cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
 
-			# naikkan consec_frame jika EAR melewati treshold
-			face_list[i]['consec_frame'] += 1
+				if ear < EYE_AR_THRESH:
 
-			# terdeteksi mengantuk pada EAR
-			if face_list[i]['consec_frame'] >= EYE_AR_CONSEC_FRAMES:
-				# alert
-				cv2.putText(frame, '(EAR)Mengantuk!', (x - 10, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+					face_list[objectID]['consec_frame'] += 1
 
-				ear_e2 = cv2.getTickCount()
-				ear_last_frame_exec_time = (ear_e2 - e1)/cv2.getTickFrequency()
-	
-				if exec_time_30_frame.full():
-					exec_time_30_frame.get()
-				exec_time_30_frame.put(ear_last_frame_exec_time)
-				
-				exec_time_list.append({
-					'timestamp': millis_convert(vs_time),
-					'id': i,
-					'detector': 'ear',
-					'time': list(exec_time_30_frame.queue)
-				})
+					if face_list[objectID]['consec_frame'] >= EYE_AR_CONSEC_FRAMES:
+						cv2.putText(frame, '(EAR)Mengantuk!', (x - 10, y - 18), cv2.FONT_HERSHEY_SIMPLEX, 
+							0.4, ((0, 0, 255)), 1)
 
-				print('{} - ID{} (ear)mengantuk'.format(vs_time, i))
+						ear_e2 = cv2.getTickCount()
+						ear_last_frame_exec_time = (ear_e2 - e1)/cv2.getTickFrequency()
+			
+						if exec_time_30_frame.full():
+							exec_time_30_frame.get()
+						exec_time_30_frame.put(ear_last_frame_exec_time)
+						
+						exec_time_list.append({
+							'timestamp': millis_convert(vs_time),
+							'id': objectID,
+							'detector': 'ear',
+							'time': list(exec_time_30_frame.queue)
+						})
 
-		else:
-			face_list[i]['consec_frame'] = 0
+				else:
+					face_list[objectID]['consec_frame'] = 0
 
-		if mar > MOUTH_AR_THRESH:
-			# alert
-			cv2.putText(frame, '(MAR)Mengantuk!', (x - 10, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+				if mar > MOUTH_AR_THRESH:
+					cv2.putText(frame, '(MAR)Mengantuk!', (x - 10, y - 18), cv2.FONT_HERSHEY_SIMPLEX, 
+						0.4, (255, 0, 0), 1)
 
-			mar_e2 = cv2.getTickCount()
-			mar_exec_time = (mar_e2 - e1)/cv2.getTickFrequency()
+					mar_e2 = cv2.getTickCount()
+					mar_exec_time = (mar_e2 - e1)/cv2.getTickFrequency()
 
-			exec_time_list.append({
-				'timestamp': millis_convert(vs_time),
-				'id': i,
-				'detector': 'mar',
-				'time': mar_exec_time
-			})
+					exec_time_list.append({
+						'timestamp': millis_convert(vs_time),
+						'id': objectID,
+						'detector': 'mar',
+						'time': mar_exec_time
+					})
 
-			print('{} - ID{} (mar)mengantuk'.format(vs_time, i))
+			else:
+				continue
 
-	cv2.imshow("Deteksi Kantuk", frame)
+	cv2.namedWindow('Deteksi Kantuk', cv2.WINDOW_NORMAL) 
+	cv2.imshow('Deteksi Kantuk', frame)
 	key = cv2.waitKey(1) & 0xFF
 	fps.update()
 
-	# selesai hitung waktu eksekusi frame
 	e2 = cv2.getTickCount()
 	exec_time = (e2 - e1)/cv2.getTickFrequency()
 	
-	# monitoring waktu eksekusi 30 frame sebelumnya
 	if exec_time_30_frame.full():
-		# keluarkan frame paling terakhir di queue
 		exec_time_30_frame.get()
 	exec_time_30_frame.put(exec_time)
 
 	if key == ord("q"):
-		# print(millis_convert(vs_time))
 		# generate_csv(exec_time_list)
 		break
 
@@ -216,4 +193,4 @@ print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 cv2.destroyAllWindows()
 vs.release()
 
-# py multi_drowsiness.py -v <path>
+# py multi_drowsiness.py -v ../dataset_to_used/dataset_variation/eight_720.mp4
